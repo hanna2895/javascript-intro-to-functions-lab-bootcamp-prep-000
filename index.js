4c5ed1c61c4b026152889e17e3de852c6c5c1dde
hanna2895 <hanna.w28@gmail.com>
2017-12-17 01:10:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,3 +13,14 @@ function logShout(string) {
 function logWhisper(string) {
   console.log(string.toLowerCase())
 }
+
+var uppercase = "HELLO!"
+var lowercase = "hello!"
+
+function sayHiToGrandma(string) {
+  if (string === lowercase) {
+    return "I can't hear you!"
+  } else {
+    doNothing()
+  }
+}
